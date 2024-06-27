0586d7a6e7106e51f5ee099289c72d640f2bf973
Yanto Young <yanto.young@rwth-aachen.de>
2024-06-27 22:25:50 +0900

    Created PostItem component to be displayed in list of posts.

---
+++ src/components/post-item.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { Calendar } from "lucide-react";
+
+interface PostItemProps {
+  id: number;
+  title: string;
+  content: string;
+  date: Date;
+}
+
+export function PostItem({
+    id,
+    title,
+    content,
+    date
+  }: PostItemProps) {
+  return (
+    <article className="flex flex-col gap-2 border-border border-b py-3">
+      <div>
+        <h2 className="font-semibold text-2xl leading-tight">
+          {title}
+        </h2>
+      </div>
+      <div className="max-w-none text-muted-foreground truncate">{content}</div>
+      <div className="flex justify-between items-center">
+        <dl>
+          <dd className="text-sm sm:text-base font-medium flex items-center gap-1">
+            <Calendar className="h-4 w-4" />
+            <time>
+              {date.toLocaleDateString(
+                "ja-JP",
+                {year: "numeric", month: "2-digit", day: "2-digit"}
+              ).replaceAll("/", "-")}
+            </time>
+          </dd>
+        </dl>
+        <Link href={`/${id}`} className="text-sm">Read more →</Link>
+      </div>
+    </article>
+  )
+}
